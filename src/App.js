664f005e8e899739b949fb41e33aa7ce8358f892
BigunetsT <tanyuschamail83@gmail.com>
2021-04-16 21:59:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
-// import Menu from './components/Menu';
+import Menu from './components/Menu';
 import './styles.css';
 
 const App = () => {
